fa7d1fe8f07ca5ec38df414d566b6d8e7619d0a3
Airconix <wsnia.dar20@gmail.com>
2025-01-13 13:52:41 +0100

    jakiś update
    niewiem chyba zchrzaniony

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,8 +1,17 @@
 extends CharacterBody2D
-class_name  Player
-
+class_name Player
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -500.0
+@onready var bullet_path = preload("res://prefabs/bullet.tscn")
+
+func die():
+	GameManager.respawn_player()
+
+func shoot():
+		var b = bullet_path.instantiate()
+		b.transform = $Muzzle.global_transform
+		owner.add_child(b)
+
 
 func _ready():
 	GameManager.player = self
@@ -15,6 +24,9 @@ func _physics_process(delta: float):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -30,5 +42,5 @@ func _physics_process(delta: float):
 		die()
 		
 
-func die():
-	GameManager.respawn_player()
+
+	
--- scripts/core/coinsUI.gd
+++ scripts/core/coinsUI.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends Node2D
 
 func _ready():
 	GameManager.gained_coins.connect(update_coin_display)
